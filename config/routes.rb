d51f47828c9f668af42d3e8df1e8608fbab0f138
Thibault Jaime <jaime.thibault@gmail.com>
2017-08-10 13:59:22 +0200

    app fonctional but no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :new, :create, :show]
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   root to: 'cocktails#index'
 end
